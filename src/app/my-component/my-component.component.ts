4f4300b5234b43942eb98e431e45f6a23d6370ed
QuocThanh <dangquocthanh2812@gmail.com>
2023-04-19 23:18:51 +0700

    SPA

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
   styleUrls: ['./my-component.component.css']
 })
-export class MyComponentComponent {
-
+export class MyComponentComponent implements OnInit {
+  todo = ["Học TypeScript", "Học Angular 4", "Học HTML5"];
+  constructor() { }
+   ngOnInit() {}
 }
